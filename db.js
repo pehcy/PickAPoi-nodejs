bd109f061d9ef4881ec0a3b3323f1a561fff4a84
pehcy <cheeyung.peh@gmail.com>
2020-12-03 13:55:39 +0800

    feat(mongodb): connect to mongoDB

---
+++ db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const MONGO_USERNAME = 'poi_user';
+const MONGO_PWD = 'root';
+const MONGO_HOSTNAME = '127.0.0.1';
+const MONGO_PORT = '27017';
+const MONGO_DB = 'poiModel';
+
+const options = { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+};
+const mongoDB = `mongodb://${MONGO_USERNAME}:${MONGO_PWD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const mongo = mongoose.connect(mongoDB, options);
+
+mongo.then(() => {
+  console.log('connected');
+}, error => {
+  console.log(error, 'error, cannot connect mongDB');
+});
